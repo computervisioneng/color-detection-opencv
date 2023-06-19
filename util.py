73beb086e121a249f072247e6cd01526b63596af
Computer vision engineer <112948275+computervisioneng@users.noreply.github.com>
2023-06-19 09:15:49 -0300

    Fix bug red hues.

--- util.py
+++ util.py
@@ -3,14 +3,20 @@ import cv2
 
 
 def get_limits(color):
-
-    c = np.uint8([[color]])  # here insert the bgr values which you want to convert to hsv
+    c = np.uint8([[color]])  # BGR values
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
-    lowerLimit = hsvC[0][0][0] - 10, 100, 100
-    upperLimit = hsvC[0][0][0] + 10, 255, 255
+    hue = hsvC[0][0][0]  # Get the hue value
 
-    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
-    upperLimit = np.array(upperLimit, dtype=np.uint8)
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit for divided red hue
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit for divided red hue
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
 
     return lowerLimit, upperLimit
